d4a885340f978b37e8e9f38d487ce741c0ac876d
Harsh Vardhan <54477764+harsh-vardhan152@users.noreply.github.com>
2022-10-14 23:33:58 +0530

    Frontend Project is created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
