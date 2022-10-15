764b8d8add727cce0ff45d1b3f44ed744a8b7b45
Gopalakrishnan20 <gopalakrishnan567890@gmail.com>
2022-10-15 17:32:24 +0530

    Patient Signup Controller & Service

--- HospitalPortal/src/main/java/com/portal/services/PatientSignUpService.java
+++ HospitalPortal/src/main/java/com/portal/services/PatientSignUpService.java
@@ -5,6 +5,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.portal.entities.PatientSignUp;
 import com.portal.repositories.PatientSignUpRepository;
 
 @Service
@@ -12,5 +13,37 @@ public class PatientSignUpService {
 	@Autowired
 	PatientSignUpRepository patientSignupRepo;
 	
+	@Transactional
+	public List<PatientSignUp> fetchPatients() {
+		List<PatientSignUp> patientsignupList=patientSignupRepo.findAll();
+		return patientsignupList;
+		
+	}
+	
+	@Transactional
+	public PatientSignUp savePatientSignUp(PatientSignUp patientsignup) {
+		return patientSignupRepo.save(patientsignup);
+		
+	}
+	
+	@Transactional
+	public void update(PatientSignUp p) {
+		patientSignupRepo.save(p);	
+	
+	}
+	
+	@Transactional
+	public void deletePatient(int p_id) {	
+		System.out.println("service method called");
+		patientSignupRepo.deleteById(p_id);
+	
+	}
+	
+	@Transactional 
+	  public PatientSignUp getPatient(int p_id) { 
+	  Optional<PatientSignUp> optional= patientSignupRepo.findById(p_id);
+	  PatientSignUp p=optional.get();
+	  return p;
+	  }
 	
 }
