7e573e6de4795b496915c6519fb6a6d9e5b73405
Kanimozhi <kanima2651@gmail.com>
2022-10-15 16:28:32 +0530

    doctor login controller and service

--- HospitalPortal/src/main/java/com/portal/repositories/DoctorLoginRepository.java
+++ HospitalPortal/src/main/java/com/portal/repositories/DoctorLoginRepository.java
@@ -6,5 +6,7 @@ import com.portal.entities.DoctorLogin;
 
 public interface DoctorLoginRepository extends JpaRepository<DoctorLogin, String>{
 
-
+	@Query("SELECT dl FROM DoctorLogin dl WHERE dl.username =?1 and dl.password=?2")
+	public DoctorLogin validateDoctorLogin(String username,String password);
+	
 }
--- HospitalPortal/src/main/java/com/portal/services/DoctorLoginService.java
+++ HospitalPortal/src/main/java/com/portal/services/DoctorLoginService.java
@@ -2,11 +2,24 @@ package com.portal.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.portal.entities.AuthenticationStatus;
+import com.portal.entities.DoctorLogin;
 import com.portal.repositories.DoctorLoginRepository;
 @Service
 public class DoctorLoginService {
 
 	@Autowired
-	DoctorLoginRepository doctorloginRepo;
+	DoctorLoginRepository doctorloginRepository;
+	public AuthenticationStatus validateDoctorLogin(String username, String password) {
+		AuthenticationStatus status = null;
+		DoctorLogin doctorlogin = doctorloginRepository.validateDoctorLogin(username, password);
+		if(doctorlogin!=null) {
+			status = new AuthenticationStatus(doctorlogin.getUsername(), doctorlogin.getPassword(), true);
+		}
+		else {
+			status = new AuthenticationStatus(null, null, false);
+		}
+		return status;
+	}
 	
 }
