fc13d94078891f782bdec2a7191618de388cee37
Kanimozhi <kanima2651@gmail.com>
2022-10-16 13:34:34 +0530

    Feedback Controller and Service

--- HospitalPortal/src/main/java/com/portal/services/FeedbackService.java
+++ HospitalPortal/src/main/java/com/portal/services/FeedbackService.java
@@ -1,17 +1,48 @@
 package com.portal.services;
 
 import java.util.List;
-import java.util.Optional;
+
 import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.portal.entities.Feedback;
 import com.portal.repositories.FeedbackRepository;
-
-
 @Service
 public class FeedbackService {
 	@Autowired
-	FeedbackRepository feedbackRepo;
-	
+	FeedbackRepository feedbackrepository;
+	@Transactional
+	public List<Feedback> fetchFeedbacks() {
+		List<Feedback> feedbacklist=feedbackrepository.findAll();
+		return feedbacklist;
+		
+	}
+	@Transactional
+	public Feedback saveFeedbacks(Feedback feedback) {
+		
+		return feedbackrepository.save(feedback);
+		
+//	}
+//	@Transactional
+//	public void updateFeedback(Feedback feedback) {
+//		feedbackrrepository.save(feedback);	
+//	
+//	}
+//	
+//	@Transactional
+//	public void deleteFeedback(int feedback_id) {
+//		System.out.println("service method called");
+//		feedbackRepository.deleteById(feedback_id);
+//	
+//	}
+//	@Transactional 
+//	  public Feedback getFeedback(int feedback_id) { 
+//	  Optional<Feedback> optional= feedbackRepository.findById(feedback_id);
+//	  Feedback feedback=optional.get();
+//	  return feedback;
+//	  }
+//}
+}
 }
