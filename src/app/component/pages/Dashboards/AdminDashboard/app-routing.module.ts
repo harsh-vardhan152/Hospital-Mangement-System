8bf43e413b5dbc8b0484e56a200d4ca2148d9ba4
durgasindhu-46 <68771781+durgasindhu-46@users.noreply.github.com>
2022-10-17 17:26:34 +0530

    routing of admin

---
+++ src/app/component/pages/Dashboards/AdminDashboard/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { PatientlistComponent } from './DoctorDashboard/patientlist/patientlist.component';
+import { PatientdischargeComponent } from './DoctorDashboard/patientdischarge/patientdischarge.component';
+import { DoctordashboardComponent } from './DoctorDashboard/doctordashboard/doctordashboard.component';
+import { NavigationComponent } from './AdminDashboard/navigation/navigation.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminDashboardsComponent } from './AdminDashboard/admin-dashboards/admin-dashboards.component';
+import { DoctorComponent } from './AdminDashboard/doctor/doctor.component';
+import { PatientComponent } from './AdminDashboard/patient/patient.component';
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    {
+      path:'dashboard',component:AdminDashboardsComponent
+    },
+    {
+      path:'doctor',component:DoctorComponent
+    },
+    {
+      path:'patient',component:PatientComponent
+    }
+];/*
+const routes:Routes=[{
+  path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    {
+      path:'dashboard',component:DoctordashboardComponent
+    },
+    {
+      path:'discharge',component:PatientdischargeComponent
+    },
+    {
+      path:'list',component:PatientlistComponent
+}
+ ];*/
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
